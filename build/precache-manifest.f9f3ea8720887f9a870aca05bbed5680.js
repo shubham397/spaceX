ced303413545d823913ee52b0c5f242b1cf35f9b
shubham <shubham@marmeto.com>
2019-11-13 00:37:46 +0530

    build

---
+++ build/precache-manifest.f9f3ea8720887f9a870aca05bbed5680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5520acc989230afe15cd019cb9230f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ccac0ea808dce3c1dc",
+    "url": "/static/css/main.8efa802c.chunk.css"
+  },
+  {
+    "revision": "790cbdbfe38507cd6871",
+    "url": "/static/js/2.625fc04b.chunk.js"
+  },
+  {
+    "revision": "11ccac0ea808dce3c1dc",
+    "url": "/static/js/main.3bb1b227.chunk.js"
+  },
+  {
+    "revision": "84a7aca42cc5c1afee5b",
+    "url": "/static/js/runtime~main.98d03f85.js"
+  },
+  {
+    "revision": "8d382ac14e84820b6b127873063fb16e",
+    "url": "/static/media/loadingImage.8d382ac1.svg"
+  },
+  {
+    "revision": "83b8277472c45d64980ddea5adf8500f",
+    "url": "/static/media/spacebackground.83b82774.jpg"
+  }
+]);
